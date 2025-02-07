00597ab841f00ec73e8776d7d0206c7f3e035532
Joao Pedro <jpv.ramalho2013@gmail.com>
2025-02-07 14:17:01 -0300

    [ADD] projeto base vue e tailwind

---
+++ projeto-base-sem-nuxt/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-blue-500 text-white p-4">
+    Hello, Tailwind CSS!
+  </div>
+  <h1 className="text-1xl font-bold underline">
+      Hello world!!
+  </h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
