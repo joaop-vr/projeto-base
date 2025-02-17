6f4227413fa02e00c959eaaa8a337ef312a54417
Joao Pedro <jpv.ramalho2013@gmail.com>
2025-02-17 14:07:02 -0300

    [ADD] vuex

--- front-end/src/pages/Home.vue
+++ front-end/src/pages/Home.vue
@@ -1,4 +1,19 @@
+<script setup lang="ts">
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+
+const store = useStore()
+
+const count = computed(() => store.state.count)
+
+const increment = () => store.commit('increment')
+const decrement = () => store.commit('decrement')
+</script>
+
 <template>
-    <h1>Página Inicial</h1>
-  </template>
-  
+  <div>
+    <h1>Contador: {{ count }}</h1>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
